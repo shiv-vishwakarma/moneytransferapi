27abfcc218b853bd641bfcea56b98fdc6b1cb8cf
Shiv Vishwakarma <shivvishwakarma.2016@gmail.com>
2022-09-06 16:11:55 +0530

    Fine-tuned Business Validation Cases

--- src/main/java/com/banking/moneytransferapi/util/BusinessValidation.java
+++ src/main/java/com/banking/moneytransferapi/util/BusinessValidation.java
@@ -1,13 +1,10 @@
 package com.banking.moneytransferapi.util;
 
 import com.banking.moneytransferapi.dto.MoneyTransferRequest;
-import com.banking.moneytransferapi.entity.BankAccountDetails;
 import com.banking.moneytransferapi.exception.BusinessValidationException;
 import com.banking.moneytransferapi.repository.BankAccountDetailsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 public class BusinessValidation {
@@ -22,14 +19,13 @@ public class BusinessValidation {
             throw new BusinessValidationException(Constants.ERROR_MSG_SAME_ACCOUNTS);
         }
 
-        //Check for Sufficient account balance
-        Optional<BankAccountDetails> bankAccountDetails = bankAccountDetailsRepo.findByAccountNumber(moneyTransferRequest.getFromAccountNumber());
-        if (bankAccountDetails.get().getAccountBalance() < moneyTransferRequest.getAmount()){
-            throw new BusinessValidationException(Constants.ERROR_MSG_INSUFFICIENT_BAL);
-        }
+        //Check if FROM ACCOUNT is present in database , if Present, then check for sufficient account balance
+        bankAccountDetailsRepo.findByAccountNumber(moneyTransferRequest.getFromAccountNumber()).ifPresentOrElse(bankAcc -> {
+            if (bankAcc.getAccountBalance() < moneyTransferRequest.getAmount()){
+                throw new BusinessValidationException(Constants.ERROR_MSG_INSUFFICIENT_BAL);
+            }
+        }, () -> {throw new BusinessValidationException(Constants.ERROR_MSG_FROM_ACCOUNT);});
 
-        //Check if FROM ACCOUNT is present in database
-        bankAccountDetailsRepo.findByAccountNumber(moneyTransferRequest.getFromAccountNumber()).orElseThrow(() -> new BusinessValidationException(Constants.ERROR_MSG_FROM_ACCOUNT));
 
         //Check if FROM ACCOUNT is present in database
         bankAccountDetailsRepo.findByAccountNumber(moneyTransferRequest.getToAccountNumber()).orElseThrow(() -> new BusinessValidationException(Constants.ERROR_MSG_TO_ACCOUNT));
